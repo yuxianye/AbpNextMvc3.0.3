b875898936320552aad184d3c2555ea3b7d8e624
yuxianye <yuxianye@163.com>
2020-06-20 18:56:58 +0800

    删除无用文件

--- aspnet-core/src/Solution.Application.Contracts/Customers/Dtos/CreateUpdateCustomerDto.cs
+++ aspnet-core/src/Solution.Application.Contracts/Customers/Dtos/CreateUpdateCustomerDto.cs
@@ -1,9 +1,13 @@
 using System;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace Solution.Customers.Dtos
 {
     public class CreateUpdateCustomerDto
     {
+        [Required]
+        [StringLength(32)]
         public string Name { get; set; }
 
         public string Address { get; set; }
